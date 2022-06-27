0214059571e4928d27d9a73397ffa96ffca7d461
lenapuzzle <lenapuzzle@yahoo.com>
2022-06-27 15:24:55 -0700

    script.js added

---
+++ JS/button_clicker/script.js
@@ -0,0 +1,10 @@
+// change Login to Logout onclick
+function changeElement(element) {
+  element.innerText = "Logout";
+}
+
+// remove Add Definition button onclick
+function removeElement(element) {
+  element.remove();
+}
+
