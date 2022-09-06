87c4dbc9052dce2f71ccc01cd9d2422615927c4a
lenapuzzle <lenapuzzle@yahoo.com>
2022-09-06 08:56:41 -0700

    mern_ordered_intersection

---
+++ algos/mern_ordered_intersection.js
@@ -0,0 +1,86 @@
+/* 
+  Efficiently combine two already sorted multiset arrays 
+  into an array containing the sorted set intersection of the two.
+  
+  Output: only the shared values between the two arrays (deduped).
+  Venn Diagram Visualization (bottom) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+*/
+
+const numsA1 = [0, 1, 2, 2, 2, 7];
+const numsB1 = [2, 2, 6, 6, 7];
+const expected1 = [2, 7];
+
+const numsA2 = [0, 1, 2, 2, 2, 7];
+const numsB2 = [2, 2];
+const expected2 = [2];
+
+const numsA3 = [0, 1, 2, 2, 2, 7];
+const numsB3 = [10];
+const expected3 = [];
+
+/**
+ * Venn Diagram Visualization (bottom):
+ * https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+ * - Time: O(n) linear, n = max(sortedA.length, sortedB.length) when there are
+ *    dupes we may end up looping over all items of longer arr.
+ * - Space: O(n) linear, n = shorter array length.
+ * @param {Array<number>} sortedA
+ * @param {Array<number>} sortedB Both sets are multisets
+ *    (multi in that it can contain multiple dupes).
+ * @returns {Array<number>} The sorted set intersection: a new array that is
+ *    sorted and contains only the shared values between the two arrays
+ *    deduped.
+ */
+
+function orderedIntersection(sortedA, sortedB) {
+    const newArray = [];
+    let arrayAPointer = 0;
+    let arrayBPointer = 0;
+
+
+    while(arrayAPointer< sortedA.length && arrayBPointer < sortedB.length){
+
+        if(arrayAPointer < sortedA.length  && sortedA[arrayAPointer] < sortedB[arrayBPointer]){
+            arrayAPointer++
+        } else if (arrayBPointer < sortedB.length  && sortedA[arrayAPointer] > sortedB[arrayBPointer]){
+            arrayBPointer++
+        } else{
+            if(newArray.includes(sortedA[arrayAPointer])){
+                arrayAPointer++
+                arrayBPointer++
+
+            }else if(sortedA[arrayAPointer]===sortedB[arrayBPointer]){
+                newArray.push(sortedA[arrayAPointer])
+                arrayAPointer++
+                arrayBPointer++
+            }else{
+                continue;
+            }
+        }
+
+    }
+    return newArray
+}
+    // // or this way
+    // function orderedIntersection(sortedA, sortedB) {
+    //     let idxA = 0;
+    //     let idxB = 0;
+    
+    //     const intersection = [];
+    
+    //     while (idxA < sortedA.length && idxB < sortedB.length) {
+    //     if (sortedA[idxA] === sortedB[idxB]) {
+    //         if (intersection[intersection.length - 1] !== sortedA[idxA]) {
+    //         // add it only if the last num added isn't the same num
+    //         intersection.push(sortedA[idxA]);
+    //         }
+    //         idxA++;
+    //         idxB++;
+    //     } else if (sortedA[idxA] < sortedB[idxB]) {
+    //         idxA++;
+    //     } else {
+    //         idxB++;
+    //     }
+    //     }
+    //     return intersection;
+    // }
